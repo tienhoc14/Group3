be94fed136cf531805fc2e01120d9de7a40dcb17
Cuong23122001 <cuongnmgch190696@fpt.edu.vn>
2022-03-10 23:24:10 +0700

    login

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
-// const session = require('express-session')
-// const { checkUserRole } = require('./databaseHandler')
-// const { requiresLogin } = require('./projectLibrary')
+const session = require('express-session')
+const { checkUserRole } = require('./databaseHandler')
+const { requiresLogin } = require('./projectLibrary')
 
 const app = express()
 
@@ -9,19 +9,70 @@ app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
 
+app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: '124447yd@@$%%#', cookie: { maxAge: 900000 }, saveUninitialized: false, resave: false }))
 
-// app.get('/', (req, res) => {
-//     res.render('login')
-// })
-// app.get('/login', (req, res) => {
-//     res.render('login')
-// })
 app.get('/', (req, res) => {
-        res.render('')
+    res.render('login')
 })
 
+app.post('/login', async(req, res) => {
+    const name = req.body.txtName
+    const pass = req.body.txtPass
+    const role = await checkUserRole(name, pass)
+    if (role == -1) {
+        res.render('login')
+    } else if (role == "Admin") {
+        req.session["Admin"] = {
+            name: name,
+            role: role
+        }
+        res.redirect('/admin')
 
-app.use(express.urlencoded({ extended: true }))
+    } else if (role == "Staff") {
+        req.session["Staff"] = {
+            name: name,
+            role: role
+        }
+        res.redirect('/staff')
+
+    } else if (role == "Trainee") {
+        req.session["Trainee"] = {
+            name: name,
+            role: role
+        }
+        res.redirect('/trainee')
+    } else if (role == "Trainer") {
+        req.session["Trainer"] = {
+            name: name,
+            role: role
+        }
+        res.redirect('/trainer')
+    }
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/logout', (req, res) => {
+    req.session.destroy()
+    res.redirect('login')
+})
+
+const adminController = require('./controllers/admin')
+app.use('/admin', adminController)
+
+const staffController = require('./controllers/staff')
+app.use('/staff', staffController)
+
+const managerController = require('./controllers/manager')
+app.use('/trainer', managerController)
+
+const coordinatorController = require('./controllers/coordinator')
+app.use('/trainee', coordinatorController)
+
+
 const PORT = process.env.PORT || 5123
 app.listen(PORT)
 console.log("Server is running! " + PORT)
