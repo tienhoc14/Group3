d06c9a005013c76c824babc943347778740402e8
Cuong23122001 <cuongnmgch190696@fpt.edu.vn>
2022-03-10 21:28:04 +0700

    start

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+// const session = require('express-session')
+// const { checkUserRole } = require('./databaseHandler')
+// const { requiresLogin } = require('./projectLibrary')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.render('login')
+})
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+
+
+app.use(express.urlencoded({ extended: true }))
+const PORT = process.env.PORT || 5123
+app.listen(PORT)
+console.log("Server is running! " + PORT)
