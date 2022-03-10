7a591ab886043472b0e3cf55b1cf506dd3dde859
tienhoc14 <87641821+tienhoc14@users.noreply.github.com>
2022-03-10 21:46:20 +0700

    ok

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 const express = require('express')
-<<<<<<< HEAD
-=======
 // const session = require('express-session')
 // const { checkUserRole } = require('./databaseHandler')
 // const { requiresLogin } = require('./projectLibrary')
 
->>>>>>> 57d6176b394089d572cbec183b033d3d692bf8a9
 const app = express()
 
 app.set('view engine', 'hbs')
 
-<<<<<<< HEAD
-app.get('/', (req, res) => {
-    res.render('index')
-})
-
-const port = 3000
-app.listen(port, () => {
-    console.log('Server is running at ' + port)
-})
-=======
 app.use(express.static('public'))
 
 
@@ -36,4 +23,3 @@ app.use(express.urlencoded({ extended: true }))
 const PORT = process.env.PORT || 5123
 app.listen(PORT)
 console.log("Server is running! " + PORT)
->>>>>>> 57d6176b394089d572cbec183b033d3d692bf8a9
