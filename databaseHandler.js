ffc4c59bb31f177ec88fbdf5bab9aeda35bca647
Cuong23122001 <cuongnmgch190696@fpt.edu.vn>
2022-03-11 10:27:21 +0700

    ok

--- databaseHandler.js
+++ databaseHandler.js
@@ -1,11 +1,13 @@
 const { MongoClient, ObjectId } = require('mongodb');
 
-// const URL = 'mongodb+srv://1670ASM:1411@cluster0.6pxwq.mongodb.net/test';
 const URL = 'mongodb://localhost:27017';
-const DATABASE_NAME = "GCH0805-ApplicationDev"
+const DATABASE_NAME = "GCH0805_1640"
 
 async function getDB() {
     const client = await MongoClient.connect(URL)
     const dbo = client.db(DATABASE_NAME);
     return dbo;
+}
+module.exports = {
+    getDB, ObjectId,
 }
